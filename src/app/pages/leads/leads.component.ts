9d2fb0f0f2aeee3fe65b724fdfc8ec0b918c0882
Shanhun-Codes <shanhun.codes@gmail.com>
2026-09-04 18:40:43 -0500

    set page title subtitles

--- src/app/pages/leads/leads.component.ts
+++ src/app/pages/leads/leads.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+import { ButtonComponent } from '../../shared/components/button/button.component';
 
 @Component({
   selector: 'aa-leads',
   standalone: true,
-  imports: [],
+  imports: [MatIcon, ButtonComponent],
   templateUrl: './leads.component.html',
-  styleUrl: './leads.component.scss'
+  styleUrl: './leads.component.scss',
 })
 export class LeadsComponent {
-
+  readonly title = 'Leads';
+  readonly subTitle = 'Manage and follow up with your open house leads here';
 }
---
+++ src/app/shared/components/button/button.config.ts
@@ -0,0 +1,7 @@
+export interface ButtonConfig {
+  label?: string;
+  icon?: string;
+  variant?: 'primary' | 'secondary' | 'danger' | 'ghost';
+  size?: 'sm' | 'md' | 'lg';
+  disabled?: boolean;
+}
