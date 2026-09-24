92be0cde0361ec8d3bf4c682e4c13c215c5258cc
Shanhun-Codes <shanhun.codes@gmail.com>
2026-09-24 17:30:34 -0500

    set qa env

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -2,8 +2,8 @@
 
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://qa-api.open-house.studio',
-  agentAppApiUrl: 'http://qa-api.open-house.studio/agent-app',
+  apiBaseUrl: 'http://api-qa.open-house.studio',
+  agentAppApiUrl: 'http://api-qa.open-house.studio/agent-app',
   publicBaseUrl: 'http://qa.open-house.studio',
   cognito: {
     clientId: '5lqcfc938nqjp20ogp05splr9l',
