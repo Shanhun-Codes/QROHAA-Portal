bcc0f1a676bb8af71881ddff3149630a7e5b0c14
Shanhun-Codes <shanhun.codes@gmail.com>
2026-09-24 18:30:44 -0500

    fix enviroment and routing guards

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -2,9 +2,9 @@
 
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://api-qa.open-house.studio',
-  agentAppApiUrl: 'http://api-qa.open-house.studio/agent-app',
-  publicBaseUrl: 'http://qa.open-house.studio',
+  apiBaseUrl: 'https://api-qa.open-house.studio',
+  agentAppApiUrl: 'https://api-qa.open-house.studio/agent-app',
+  publicBaseUrl: 'https://qa.open-house.studio',
   cognito: {
     clientId: '5lqcfc938nqjp20ogp05splr9l',
   },
